a04558a32d45085842a684b59c2b71d38c462421
LuketoDev <lucasbravouni@gmail.com>
2024-08-30 19:51:57 -0300

    Clase Vehiculo y Persona testeo verde (completados)

---
+++ src/ar/edu/unlam/pb2/dominio/practica/ejercicio1/Vehiculo.java
@@ -0,0 +1,57 @@
+package ar.edu.unlam.pb2.dominio.practica.ejercicio1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vehiculo {
+
+	private String pantente; // String porque puede tener letras
+	private Integer pesoMaximo; // en kg
+	private Integer capacidadMaximaDePasajeros;
+	private List<Pasajero> pasajeros;
+
+	public Vehiculo(String patente, Integer pesoMaximo, Integer capacidadMaximaDePasajeros) {
+		// TODO Auto-generated constructor stub
+		this.pantente = patente;
+		this.pesoMaximo = pesoMaximo;
+		this.capacidadMaximaDePasajeros = capacidadMaximaDePasajeros;
+		this.pasajeros = new ArrayList<>();
+	}
+
+	public String getPatente() {
+		// TODO Auto-generated method stub
+		return this.pantente;
+	}
+
+	public Integer obtenerPesoMaximo() {
+		// TODO Auto-generated method stub
+		return this.pesoMaximo;
+	}
+
+	public Integer obtenerCapacidadMaximaDePasajeros() {
+		// TODO Auto-generated method stub
+		return this.capacidadMaximaDePasajeros;
+	}
+
+	public void cargarPasajeros(List<Pasajero> pasajerosASubir) {
+		
+		for (Pasajero pasajero : pasajerosASubir) {
+			
+			this.pasajeros.add(pasajero);
+			
+		}
+		
+	}
+	
+	public Boolean cargarPasajero(Pasajero pasajeroASubir) {
+		return this.pasajeros.add(pasajeroASubir);
+	}
+
+	public List<Pasajero> getPasajeros() {
+		// TODO Auto-generated method stub
+		return this.pasajeros;
+	}
+
+	
+
+}
---
+++ test/ar/edu/unlam/pb2/dominio/practica/ejercicio1/Pasajero.java
@@ -0,0 +1,9 @@
+package ar.edu.unlam.pb2.dominio.practica.ejercicio1;
+
+public class Pasajero {
+
+	public Pasajero(int peso) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
